1c242fb1fc70232d98a80d6d0db8fb1ee9118ef8
KothakapuShivaRamReddy <shivakothakapu191818@gmail.com>
2025-04-20 18:38:47 +0530

    The demo audio file is added

--- public/app.js
+++ public/app.js
@@ -1,16 +1,21 @@
-const audio=document.getElementById("audio");
-const playbtn=document.getElementById("playbtn");
-let isplaying=false;
-playbtn.addEventListener("click",()=>{
-    if(isplaying){
-        audio.pause();
-        playbtn.textContent="hello";
-    }else{
-        audio.play();
-        playbtn.textContent="⏸";
-    }
-    isplaying=!isplaying;
-});
-
+        const audio=document.querySelectorAll("#audio");
+        const btn=document.querySelectorAll("#playbtn");
+        let isplaying=false;
+    btn.forEach((b)=>{  b.textContent="▶";
+        b.addEventListener("click", () => {
+            if(isplaying){
+                audio.forEach((a)=>{ a.pause();});
+                b.textContent = "▶";
+            }else{
+                audio.forEach((a)=>{ a.play();});
+                 b.textContent = "⏸";
+            } isplaying=!isplaying;
+            // if (b.textContent === "▶") {
+            //     b.textContent = "⏸";
+            // } else {
+            //     b.textContent = "▶";
+            }); 
+        });
+       
 
 // module.exports=app;
